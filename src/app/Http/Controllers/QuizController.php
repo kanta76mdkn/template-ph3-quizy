52b29b2bba22dfafaa298850273af9975c913932
Kanta Ono <kanta20021476@gmail.com>
2022-08-07 13:01:32 +0900

    問題の小問が表示されるようになった！

--- src/app/Http/Controllers/QuizController.php
+++ src/app/Http/Controllers/QuizController.php
@@ -17,15 +17,32 @@ class QuizController extends Controller
         $links = DB::table('big_questions')->get();
         // list.blade.php で foreach を回し問題リストを表示させる
         // 階層下がるときは.を使う
-        return view('quiz.list',compact('links'));
+        return view('quiz.list', compact('links'));
 
-        
+
         // $choices = DB::table('choices')->get();
         // return view('quiz/quiz1', ['choices' => $choices]);
     }
     public function quiz($id)
-    {   
-        $questions = DB::table('questions')->where('big_question_id',$id)->get();
-        return view('quiz.quiz',compact('questions'));
+    {
+        $questions = DB::table('questions')->where('big_question_id',$id)
+            ->get();
+        $choices = DB::table('questions')
+            ->join('choices', 'choices.question_id', '=', 'questions.id')
+            ->where('big_question_id', '=', $id)
+            ->get()
+            ->groupBy('question_id'); // コレクションのgroupByなので勘違いしないようにお願いします
+// dd($questions);
+// dd($choices);
+
+        return view('quiz.quiz', compact('questions','choices'));        // ->join('choices', 'choices.question_id', '=', 'questions.id')
+
+        // ->where('questions.id', '=', $id)
+        // ->get()
+        // ->groupBy('question_id'); // コレクションのgroupByなので勘違いしないようにお願いします
+
     }
+    // 選択肢？回答？をjoinして選択肢分の設問を取得 (絞り込み条件に大問1を今回は指定) 選択肢 = choices, 設問 = questions, 大問 = big_questions
+
+    // 取得した後に設問毎にグルーピングしたリストに加工
 }
--- src/resources/views/quiz/quiz.blade.php
+++ src/resources/views/quiz/quiz.blade.php
@@ -9,8 +9,16 @@
 <body>
     @foreach ($questions as $question)
     <h1>{{$loop->iteration}}.この地名はなんて読む?</h1>
+    <img src="{{asset('/img/'.$question->image)}}" alt="">
+
     <!-- ダブルクオートの中だったら変数展開できるよ -->
-    <img src='{{asset("img/$question->image")}}' alt="">
+    @foreach($choices as $choice)
+    <!-- choices Tableの中は配列で帰ってきている
+    　→配列番号ごとに返ってくるようにした。
+    　→choices Tableには番号しか元々なかった
+    　→番号の中に入っている情報を持ってくるようにした。 -->
+    <div>{{$choice[ $loop->index ]->name}}</div>
+    @endforeach
     @endforeach
 </body>
 </html>
